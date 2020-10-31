c59dd2277ac0d4aa3c0a828d5f27846f54a7fe22
Juliolatorre <julio.c.latorre@gmail.com>
2020-10-31 12:08:44 -0300

    Next Level Week#2

--- src/database/test.js
+++ src/database/test.js
@@ -1,19 +1,12 @@
 const Database =  require('./db')
 const createProffy = require('./createProffys')
 
-
 Database.then(async (db) => {
     proffyValue = {
         name:"Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "19884561239",
-<<<<<<< HEAD
-        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-=======
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
->>>>>>> 96b73b4aac278ee5661470ac79c1ca012b6f49a6
-        subject: "Química",
-       
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     }
 
     classValue = {
@@ -35,5 +28,28 @@ Database.then(async (db) => {
         }
    
     ]
-   await createProffy(db, {proffyValue, classValue, classScheduleValues})
+  
+ // await createProffy(db, { proffyValue, classValue, classScheduleValues})
+
+  const selectedProffys = await db.all("SELECT * FROM proffys")
+  // console.log(selectedProffys)
+
+  const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1
+  `)
+  // console.log(selectClassesAndProffys)
+
+  const selectClassesSchedules = await db.all(`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = "1"
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "520"
+    AND class_schedule.time_to > "520"
+  `) 
+
+  // console.log(selectClassesSchedules)
 })
